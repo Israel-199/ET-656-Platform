b82e7ecc32b5fcb0a5488723d9950aad2beef6af
Israel-199 <israelassefa199@gmail.com>
2026-07-03 05:42:19 -0700

    add the deployed api

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const BASE_URL=import.meta.env.MODE === "development" ? "http://localhost:3000/api":"/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : "https://et-656-platform-1.onrender.com/api";
+    
 export const axiosInstance =axios.create({
     baseURL:BASE_URL,
     withCredentials:true,
