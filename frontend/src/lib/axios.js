83af7dff2a970dd013d7b71459aa200aa34d5bf6
Israel-199 <israelassefa199@gmail.com>
2026-07-27 08:43:37 -0700

    feat: initialize backend project with essential dependencies and scripts

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000/api"
-    : "https://et-656-platform-1.onrender.com/api";
+    : "https://et-656-platform-9ko0.onrender.com/api";
     
 export const axiosInstance =axios.create({
     baseURL:BASE_URL,
